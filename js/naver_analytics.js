9186e58dc404a869f3fce7770d18dc3013603d0e
ko-hoon <seunghoon_jeon@kolon.com>
2026-01-22 08:47:16 +0900

    naver analytics javascript add

---
+++ js/naver_analytics.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="//wcs.pstatic.net/wcslog.js"></script>
+<script type="text/javascript">
+    if (!wcs_add) var wcs_add = {};
+    wcs_add["wa"] = "6727a4a95b6a08";
+    if (window.wcs) {
+        wcs_do();
+    }
+</script>
